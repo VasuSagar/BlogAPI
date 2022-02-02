f5557738190e0044687928be6817bb7c12950a1b
Vasu Sagar <vasu.sagar2605@gmail.com>
2022-02-02 14:19:52 +0530

    Initial Commit and Added necessay packes in pom.xml

---
+++ src/test/java/com/neon/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neon.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
